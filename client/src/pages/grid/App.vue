12e935b039a5ad4fdd41538c472486b20585a5ea
Brandon Govindarajoo <bgovi@umich.edu>
2023-05-17 13:19:31 -0400

    main layout built for grid component

--- client/src/pages/grid/App.vue
+++ client/src/pages/grid/App.vue
@@ -1,17 +1,14 @@
 <template>
-<div>
+<div  >
   <nav-bar />
 
-  <div class="is-widescreen">
-    <div class="columns">
+  <div class="is-widescreen" >
+    <div class="columns" style="max-height: calc(100vh - 2.8rem)" >
       <side-menu />
 
-      <div class="column content">
-          <h1 class="title">Section</h1>
-          <h2 class="subtitle">
-            A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
-          </h2>
-      </div>
+      <div class="column content" style="height: calc(100vh - 2.72rem)">
+        <grid />
+      </div >
     </div>
 
   </div>
@@ -27,12 +24,14 @@
 <script>
 
 // https://jsbin.com/wejomuf/edit?html,css,output
+// https://github.com/jgthms/bulma/issues/2560
 
-import 'bulma/css/bulma.css'
+
+// import 'bulma/css/bulma.css'
 
 import NavBar   from './components/NavBar.vue'
 import SideMenu from './components/SideMenu.vue'
-
+import Grid from     './components/Grid.vue'
 
 
 
@@ -44,7 +43,8 @@ export default {
   },
   components: {
     NavBar,
-    SideMenu
+    SideMenu,
+    Grid
   },
 
 
@@ -56,22 +56,29 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+// body {
+//   overflow-y: hidden; /* Hide vertical scrollbar */
+//   overflow-x: hidden; /* Hide horizontal scrollbar */
+// }
+// @import 'bulma/css/bulma.css';
+
+@import "./assets/bulma.scss";
+
 .menu {
   position: sticky;
   display: inline-block;
   vertical-align: top;
   max-height: 100vh;
   overflow-y: auto;
-  width: 200px;
+  width: 230px;
   top: 0;
   bottom: 0;
-  padding: 30px;
+  padding: 20px;
 }
 
 .content {
   display: inline-block;
 }
-
 
 </style>
--- client/vue.config.js
+++ client/vue.config.js
@@ -2,23 +2,29 @@ module.exports = {
 
     pages: {
         index: {
-        entry: './src/pages/index/main.js',
-        template: 'public/index.html',
-        title: 'index page',
-        chunks: ['chunk-vendors', 'chunk-common', 'index']
+            entry: './src/pages/index/main.js',
+            template: 'public/index.html',
+            title: 'index page',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
         },
         admin: {
-        entry: './src/pages/admin/main.js',
-        template: 'public/index.html',
-        title: 'admin page',
-        chunks: ['chunk-vendors', 'chunk-common', 'admin']
+            entry: './src/pages/admin/main.js',
+            template: 'public/index.html',
+            title: 'admin page',
+            chunks: ['chunk-vendors', 'chunk-common', 'admin']
         },
         hello: {
             entry: './src/pages/hello/main.js',
             template: 'public/index.html',
             title: 'admin page',
             chunks: ['chunk-vendors', 'chunk-common', 'hello']
-        }
+        },
+        grid: {
+            entry: './src/pages/grid/main.js',
+            template: 'public/index.html',
+            title: 'grid page',
+            chunks: ['chunk-vendors', 'chunk-common', 'grid']
+        },
     }
     //devServer and devServer.proxy
     //nodemon for server
